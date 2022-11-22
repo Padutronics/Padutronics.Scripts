43938ab0c87af1e1baca3ef818315e337c8b8b96
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-11-22 22:56:12 +0200

    Add script for publishing NuGet package

---
+++ PublishNuGetPackage.ps1
@@ -0,0 +1,30 @@
+$PadutronicsPushPackageApiKeyName = "PadutronicsPushPackageApiKey"
+
+if (Test-Path env:$PadutronicsPushPackageApiKeyName) {
+    $ApiKey = (Get-Item env:$PadutronicsPushPackageApiKeyName).Value
+    $Configuration = "Debug"
+    $SourceName = "Padutronics"
+
+    $CurrentDirectory = Get-Location
+    $ProjectName = Split-Path $CurrentDirectory -Leaf
+
+    $OutputDirectory = "bin"
+    $ProjectDirectory = "${CurrentDirectory}/Source/${ProjectName}"
+    $ProjectFile = "${ProjectName}.csproj"
+
+    Push-Location $ProjectDirectory
+
+    $ProjectFileXml = [xml](Get-Content $ProjectFile)
+    $PackageVersion = $ProjectFileXml.Project.PropertyGroup.Version
+
+    Write-Host "Publishing version ${PackageVersion}" -ForegroundColor Magenta
+
+    dotnet pack $ProjectFile --configuration $Configuration --include-source --include-symbols --output "${OutputDirectory}/${Configuration}"
+
+    dotnet nuget push "${OutputDirectory}/${Configuration}/${ProjectName}.${PackageVersion}.symbols.nupkg" --api-key $ApiKey --source $SourceName
+
+    Pop-Location
+}
+else {
+    Write-Host "Environment variable '${PadutronicsPushPackageApiKeyName}' is not found" -ForegroundColor Red
+}
