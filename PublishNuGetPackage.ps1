3b7ca1b536ac657cd7394291fdce10eba7e860ac
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-11-27 23:10:25 +0200

    Add comments to PublishNuGetPackage.ps1 script

--- PublishNuGetPackage.ps1
+++ PublishNuGetPackage.ps1
@@ -28,9 +28,11 @@ begin {
 }
 
 process {
+    # Check that environment variable that container API key is present.
     if (Test-Path env:$PadutronicsPushPackageApiKeyName) {
         $ApiKey = (Get-Item env:$PadutronicsPushPackageApiKeyName).Value
 
+        # Get current version from the project file.
         $ProjectFileName = "$ProjectName.csproj"
         $ProjectFilePath = "$ProjectDirectory/Source/$ProjectName/$ProjectFileName"
         $ProjectOutputDirectory = "$ProjectDirectory/Source/$ProjectName/bin/$Configuration"
@@ -38,6 +40,7 @@ process {
         $ProjectFileXml = [xml](Get-Content $ProjectFilePath)
         $PackageVersion = $ProjectFileXml.Project.PropertyGroup.Version
 
+        # Pack a NuGet package and publish it.
         Write-Host "Publishing version $PackageVersion" -ForegroundColor Magenta
 
         dotnet pack $ProjectFilePath --configuration $Configuration --include-source --include-symbols --output $ProjectOutputDirectory
