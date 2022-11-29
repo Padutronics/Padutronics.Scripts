a8faaf7af5e675fe283427f5d0d7da22a8c4a58d
Pavel Dubsky <pavel.dubsky@padutronics.com>
2022-11-29 21:10:43 +0200

    Add comments to NewProject.ps1 script

--- NewProject.ps1
+++ NewProject.ps1
@@ -16,10 +16,10 @@ param (
 begin {
     $ErrorActionPreference = 'Stop'
 
+    # Load external scripts.
     . $PSScriptRoot/Functions/FormatJson.ps1
 
-    # Constants
-
+    # Declare constants.
     $GitHubTokenName = 'GH_TOKEN';
 
     $ProjectFileUrl = 'https://gist.githubusercontent.com/ppdubsky/8fa9c4222ca3043aa5ebd4d51c91a4a4/raw/542581333c4a65b62e5bec183520447f76709436/ClassLibrary.csproj'
@@ -31,8 +31,7 @@ begin {
     $BumpVersionPatchTaskUrl = 'https://gist.githubusercontent.com/ppdubsky/3376dd4bb80796f42430fa8e13e20329/raw/8dfb6c1b8201233345baef221ada517a08b713da/tasks.json-bump-version-patch'
     $PublishTaskUrl = 'https://gist.githubusercontent.com/ppdubsky/d2fae75712c806ce157ff3399ccff6cf/raw/1737f6b67a4ec4116aca278de0ca4ecbca3c70e2/tasks.json-publish'
 
-    # Process parameters
-
+    # Process parameters.
     if ($PSBoundParameters.ContainsKey('ProjectDirectory')) {
         $ProjectDirectory = $ProjectDirectory | Resolve-Path
     }
@@ -49,16 +48,14 @@ process {
     if (Test-Path env:$GitHubTokenName) {
         Push-Location $ProjectDirectory
 
-        # Add .gitignore
-
+        # Add .gitignore.
         New-Item -Path . -Name '.gitignore'
 
         git init
         git add .gitignore
         git commit -m 'Add .gitignore'
 
-        # Create a project
-
+        # Create a project.
         $ProjectFilePath = "$ProjectDirectory/Source/$ProjectName/$ProjectName.csproj"
 
         New-Item -ItemType 'Directory' -Path . -Name Source
@@ -105,8 +102,7 @@ process {
         git commit -m "Add project $ProjectName"
         git tag v0.0.0
 
-        # Add Visual Studio Code tasks
-
+        # Add Visual Studio Code tasks.
         $BumpVersionMajorTask = Invoke-WebRequest -Uri $BumpVersionMajorTaskUrl | ConvertFrom-Json
 
         $Json.tasks += $BumpVersionMajorTask
@@ -133,14 +129,12 @@ process {
         git add .
         git commit -m 'Add Visual Studio Code task for publishing NuGet package'
 
-        # Create GitHub project
-
+        # Create a GitHub project.
         gh repo create Padutronics/$ProjectName --public
 
         git remote add origin git@github.com:Padutronics/$ProjectName.git
 
-        # Setup git branches
-
+        # Setup git branches.
         git branch develop main
         git push --set-upstream origin main
         git push --set-upstream origin develop
